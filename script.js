5ec160780171b95d05c8330b741f808053dba183
Sanidhya786 <kumarsanidhya02@gmail.com>
2024-02-08 01:32:11 +0530

    changed

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var startBtn = document.querySelector("button")
 var allElems = document.querySelectorAll(".elem")
 var playGround = document.querySelector(".playground")
 var selected = ""
+var scoreValue = document.querySelector(".time-score h5 span")
+var score = 0;
 
 
 startBtn.addEventListener("click",function(){
@@ -18,54 +20,63 @@ allElems.forEach(function(elem){
         selected = elem.childNodes[3].getAttribute("src")
         screen[2].style.transform = "translateY(-200%)";
         
-        // createImage();
+    
+        // ye isliye diya h taki hr 1000 miliseconds me image create hogi 
+        setInterval(function(){
+            createImage()
+        },1000)
        
-        addImage();
+        
     })
 })
-
 
 
 function createImage(){
     var newImg = document.createElement("img");   
     newImg.setAttribute("src",selected);
     
-        var obj = getRandom()
-        newImg.style.left = obj.x+"%";
-        newImg.style.top = obj.y+"%";
-        newImg.style.rotate = obj.rot+"deg";
+    var x = Math.floor(Math.random()*100);
+    var y = Math.floor(Math.random()*100);
+    var rot = Math.floor(Math.random()*360);
+    newImg.style.left = x+"%";
+    newImg.style.top = y+"%";
+    newImg.style.rotate = rot+"deg";
         
-        playGround.appendChild(newImg);
-        
-        
-        catchImage()
-    }
-    
-   
-    function addImage(){
-        setTimeout(createImage,2000)
-    }
-    
-    function getRandom(){
-        var x = Math.floor(Math.random()*100);
-        var y = Math.floor(Math.random()*100);
-        var rot = Math.floor(Math.random()*360);
-        
-        return {x,y,rot}
-    }
-    // Note:-   kuchh concept smjhne k liye class4,5 dekhen  
-    
-    
-    function catchImage(){
-    var playImage = document.querySelector(".playground img")
+    playGround.appendChild(newImg);
+     
+    //  ye hr 1200 miliseconds me image remove krega.
+    setTimeout(function(){
+        //  "removeChild" child ko remove kr deta h
+        playGround.removeChild(newImg)
+    },1200)
 
-        playImage.addEventListener("click",function(){
-            var obj = getRandom()
-            
-           
-        })
-    }
-    
+    catchImage();
+}
+  
+
+function catchImage(){
+    document.querySelector(".playground img").addEventListener("click",function(){
+        score++;
+        scoreValue.innerHTML = score;
+    })
+}   
+
+
+
+// function catchImage(){
+//     var image = document.querySelector(".playground img")
+//     image.forEach(function(elem){
+//         elem.addEventListener("click",function(){
+
+//             score++;
+//             scoreValue.innerHTML = score;
+//         })
+
+
+//     })
+// }   
+
+
     
     
 
